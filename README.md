2b87ba7965f4ccf3f47e6ef123fb97c916980b45
Fanda Fan <fanfanda@ict.ac.cn>
2023-09-02 18:30:18 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# Hierarchical Masked 3D Diffusion Model for Video Outpainting
+
+This is the repository that contains source code for the paper "Hierarchical Masked 3D Diffusion Model for Video Outpainting". [[Paper](https://doi.org/10.1016/j.jvcir.2022.103686)][[Website](https://fanfanda.github.io/M3DDM/)]
+ 
+![Screenshot](assets/teaser.gif)
+
+## Citation
+```bash
+@article{HU2022103686,
+title = {Decomposing style, content, and motion for videos},
+journal = {Journal of Visual Communication and Image Representation},
+volume = {89},
+pages = {103686},
+year = {2022},
+issn = {1047-3203},
+doi = {https://doi.org/10.1016/j.jvcir.2022.103686},
+url = {https://www.sciencedirect.com/science/article/pii/S1047320322002061},
+author = {Yaosi Hu and Dacheng Yin and Yuwang Wang and Zhenzhong Chen and Chong Luo}
+}
+```
+
---
+++ static/js/index.js
@@ -0,0 +1,151 @@
+window.HELP_IMPROVE_VIDEOJS = false;
+
+
+$(document).ready(function() {
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+    });
+
+
+		// <--generation-carousel-->
+    var options = {
+			slidesToScroll: 1,
+			slidesToShow: 1,
+			loop: true,
+			infinite: true,
+			autoplay: false,
+			autoplaySpeed: 3000,
+    }
+
+		// Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('.results-carousel', options);
+
+    // Loop on each carousel initialized
+    for(var i = 0; i < carousels.length; i++) {
+    	// Add listener to  event
+    	carousels[i].on('before:show', state => {
+    		console.log(state);
+    	});
+    }
+		// </--generation-carousel-->
+		
+		// <--spatret-carousel-->
+		var options = {
+			slidesToScroll: 1,
+			slidesToShow: 1,
+			loop: true,
+			infinite: true,
+			autoplay: false,
+			autoplaySpeed: 3000,
+    }
+
+	  // Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('.spatret-carousel', options);
+
+    // Loop on each carousel initialized
+    for(var i = 0; i < carousels.length; i++) {
+    	// Add listener to  event
+    	carousels[i].on('before:show', state => {
+    		console.log(state);
+    	});
+    }
+		// </--spatret-carousel-->
+
+
+		// <--comparison-carousel-->
+		var options = {
+			slidesToScroll: 1,
+			slidesToShow: 1,
+			loop: true,
+			infinite: true,
+			autoplay: false,
+			autoplaySpeed: 3000,
+    }
+
+	  // Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('.comparison-carousel', options);
+
+    // Loop on each carousel initialized
+    for(var i = 0; i < carousels.length; i++) {
+    	// Add listener to  event
+    	carousels[i].on('before:show', state => {
+    		console.log(state);
+    	});
+    }
+		// </--comparison-carousel-->
+
+		
+		// <--tempret-carousel-->
+		var options = {
+			slidesToScroll: 1,
+			slidesToShow: 3,
+			loop: true,
+			infinite: true,
+			autoplay: false,
+			autoplaySpeed: 3000,
+    }
+
+	  // Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('.tempret-carousel', options);
+
+    // Loop on each carousel initialized
+    for(var i = 0; i < carousels.length; i++) {
+    	// Add listener to  event
+    	carousels[i].on('before:show', state => {
+    		console.log(state);
+    	});
+    }
+		// </--tempret-carousel-->
+
+
+
+		// <--cinp-carousel-->
+		var options = {
+			slidesToScroll: 1,
+			slidesToShow: 3,
+			loop: true,
+			infinite: true,
+			autoplay: false,
+			autoplaySpeed: 3000,
+    }
+
+	  // Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('.cinp-carousel', options);
+
+    // Loop on each carousel initialized
+    for(var i = 0; i < carousels.length; i++) {
+    	// Add listener to  event
+    	carousels[i].on('before:show', state => {
+    		console.log(state);
+    	});
+    }
+		// </--cinp-carousel-->
+
+
+    // Access to bulmaCarousel instance of an element
+    var element = document.querySelector('#my-element');
+    if (element && element.bulmaCarousel) {
+    	// bulmaCarousel instance is available as element.bulmaCarousel
+    	element.bulmaCarousel.on('before-show', function(state) {
+    		console.log(state);
+    	});
+    }
+		
+				
+		// add Next-Prev to sliders
+		
+		nexts = $(".slider-navigation-next");
+		for(var i=0;i<nexts.length;i++){
+				nexts[i].innerHTML += "<b>Next</b>";
+		}
+		prevs = $(".slider-navigation-previous");
+		for(var i=0;i<prevs.length;i++){
+				prevs[i].innerHTML += "<b>Prev</b>";
+		}
+
+
+})
